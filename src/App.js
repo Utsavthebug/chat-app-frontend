40045e7a7d2cce245e1c9b2015f2708d2f5f7a50
Utsavthebug <neupaneutsav.2001@gmail.com>
2023-11-14 12:19:39 +0545

    react router dom added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import {DocumentIcon} from './svg'
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
 
 function App() {
   return (
-    <div className='dark'>
+    <div className="dark">
+    <Router>
+      <Routes>
+      <Route exact path="/" element={<Home/>}  />
+      <Route exact path="/login" element={<Login/>}  />
+      <Route exact path="/register" element={<Register/>}  />
+      </Routes>
+    </Router>
     </div>
   );
 }
